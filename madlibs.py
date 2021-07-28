078e207e03b8ead0e9984cf0256910c9b210112b
sierra hughey <sierrahughey@mailbox.org>
2021-07-28 21:12:17 +0000

    inital commit

---
+++ madlibs.py
@@ -0,0 +1,86 @@
+"""A madlib game that compliments its users."""
+
+from random import choice
+
+from flask import Flask, render_template, request
+
+# "__name__" is a special Python variable for the name of the current module.
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful',
+    'smashing', 'lovely',
+]
+
+
+@app.route('/')
+def start_here():
+    """Display homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route('/greet')
+def greet_person():
+    """Greet user with compliment."""
+
+    player = request.args.get("person")
+
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html",
+                           person=player,
+                           compliment=compliment)
+
+@app.route('/game')
+def show_madlib_form():
+    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+    animals = ['badger mole', 'platypus bear', 'turtle duck', 'flying bison', 'cat', 'dog']
+    cloths =['towel', 'cloth', 'napkin', 'paper towel', 'sponge', 'sea sponge']
+    drinks = ['soda', 'water', 'tea', 'coffee', 'milk']
+    extinguishers = ['a chair', 'a computer', 'a cup of water', 'a box', 'some files']
+    wants_game = request.args.get("game")
+    if wants_game == "True":
+        return render_template("game.html", 
+                                colors=colors,
+                                animals=animals,
+                                cloths=cloths,
+                                drinks=drinks,
+                                extinguishers=extinguishers)
+    else:
+        return render_template("goodbye.html")
+
+@app.route("/madlib")
+def show_madlib():
+    person = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+    cloth = request.args.get("cloth")
+    drink = request.args.get("drink")
+    extinguishers = request.args.getlist("extinguishers")
+
+    return render_template("madlib.html", 
+                            person = person, 
+                            color = color,  
+                            noun = noun, 
+                            adjective = adjective,
+                            cloth=cloth,
+                            drink=drink,
+                            extinguishers=extinguishers)
+
+
+if __name__ == '__main__':
+    # Setting debug=True gives us error messages in the browser and also
+    # "reloads" our web app if we change the code.
+
+    app.run(debug=True, host="0.0.0.0")
